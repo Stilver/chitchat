5c8dcaa6e78b7ec1245169971f4c0876d9bebc20
steven nikolaev <stilver8@gmail.com>
2021-05-08 12:30:13 +0500

    css styles for profile page

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Messages(models.Model):
+    text = models.TextField()
+    created_on = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
